92075ae1c6b5890f2c2e3b05d62c1fb428f3595c
cir52 <47673425+cir52@users.noreply.github.com>
2023-07-18 13:38:26 -0300

    changed shadow position

--- react/src/canvas/Backdrop.jsx
+++ react/src/canvas/Backdrop.jsx
@@ -15,17 +15,17 @@ const Backdrop = () => {
       position={[0, 0, -0.14]}
     >
       <RandomizedLight 
-        amount={4}
-        radius={2}
-        intensity={0.75}
+        amount={2}
+        radius={10}
+        intensity={0.35}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight 
-        amount={8}
-        radius={3}
+        amount={2}
+        radius={6}
         intensity={0.55}
-        ambient={0.05}
+        ambient={0.55}
         position={[-5, 5, -9]}
       />
     </AccumulativeShadows>    
